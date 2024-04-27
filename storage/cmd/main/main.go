cf289cdd32e2df9fd86e8d2667236f1e68aa83c7
danil <s3menovd4nil@yandex.ru>
2024-04-27 17:01:59 +0400

    refactor: configs

--- storage/cmd/main/main.go
+++ storage/cmd/main/main.go
@@ -3,24 +3,18 @@ package main
 import (
 	"context"
 	"fmt"
-	pgxLogrus "github.com/jackc/pgx-logrus"
-	"github.com/jackc/pgx/v5/pgxpool"
-	"github.com/jackc/pgx/v5/tracelog"
-	"github.com/joho/godotenv"
-	"github.com/redis/go-redis/v9"
 	"github.com/sirupsen/logrus"
 	"golang.org/x/sync/errgroup"
 	"os"
 	"os/signal"
 	"storage/internal/adapters/kafka"
-	"storage/internal/adapters/postgres"
-	rds "storage/internal/adapters/redis"
 	"storage/internal/app"
 	"storage/internal/config"
 	"storage/internal/repository"
-	"strings"
 	"syscall"
 )
+
+const EnvFile = "example.env"
 
 func main() {
 	logger := &logrus.Logger{
@@ -34,13 +28,7 @@ func main() {
 		Level: logrus.DebugLevel,
 	}
 
-	if err := godotenv.Load(); err != nil {
-		logger.
-			WithError(err).
-			Error("cannot load .env file:")
-	}
-
-	postgresConfig, kafkaConfig, redisConfig, err := getConfigs(logger)
+	postgresConfig, kafkaConfig, redisConfig, err := config.Get(logger, EnvFile)
 	if err != nil {
 		logger.Fatal(err)
 	}
@@ -85,62 +73,4 @@ func main() {
 			WithError(err).
 			Error("failed to close consumer")
 	}
-}
-
-func getConfigs(logger *logrus.Logger) (*postgres.Config, *kafka.Config, *rds.Config, error) {
-	cfg, err := config.Get()
-	if err != nil {
-		logger.
-			WithError(err).
-			Error("cannot get config")
-		return nil, nil, nil, err
-	}
-	pgxConfig, err := pgxpool.ParseConfig(
-		fmt.Sprintf("postgres://%s:%s@%s:%s/%s",
-			cfg.Postgres.Username,
-			cfg.Postgres.Password,
-			cfg.Postgres.Host,
-			cfg.Postgres.Port,
-			cfg.Postgres.Database,
-		),
-	)
-	if err != nil {
-		logger.
-			WithError(err).
-			Error("cannot parse postgres config")
-		return nil, nil, nil, err
-	}
-	pgxConfig.ConnConfig.Tracer = &tracelog.TraceLog{
-		Logger:   pgxLogrus.NewLogger(logger.WithField("FROM", "[PGX-POOL]")),
-		LogLevel: tracelog.LogLevelDebug,
-	}
-
-	pool, err := pgxpool.NewWithConfig(context.Background(), pgxConfig)
-	if err != nil {
-		logger.
-			WithError(err).
-			Error("cannot create pgx pool")
-		return nil, nil, nil, err
-	}
-	postgresConfig := &postgres.Config{
-		Pool:   pool,
-		Logger: logger.WithField("FROM", "[POSTGRES]"),
-	}
-
-	kafkaConfig := &kafka.Config{
-		Brokers: strings.Split(cfg.Kafka.Brokers, ","),
-		Topics:  strings.Split(cfg.Kafka.Topics, ","),
-		GroupID: cfg.Kafka.GroupID,
-		Logger:  logger.WithField("FROM", "[KAFKA-CONSUMER]"),
-	}
-	redisConfig := &rds.Config{
-		Opt: &redis.Options{
-			Addr: fmt.Sprintf("%s:%s", cfg.Redis.Host, cfg.Redis.Port),
-			DB:   cfg.Redis.DB,
-		},
-		Key:    cfg.Redis.Key,
-		Logger: logger.WithField("FROM", "[REDIS]"),
-	}
-
-	return postgresConfig, kafkaConfig, redisConfig, nil
 }
--- storage/internal/config/config.go
+++ storage/internal/config/config.go
@@ -1,27 +1,107 @@
 package config
 
-type Config struct {
+import (
+	"context"
+	"fmt"
+	pgxLogrus "github.com/jackc/pgx-logrus"
+	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/jackc/pgx/v5/tracelog"
+	"github.com/joho/godotenv"
+	"github.com/redis/go-redis/v9"
+	"github.com/sirupsen/logrus"
+	"storage/internal/adapters/kafka"
+	"storage/internal/adapters/postgres"
+	rds "storage/internal/adapters/redis"
+	"strings"
+)
+
+type config struct {
 	Kafka
 	Postgres
 	Redis
 }
 
-func Get() (*Config, error) {
-	kafka, err := getKafkaConfig()
+func Get(logger *logrus.Logger, envFile string) (*postgres.Config, *kafka.Config, *rds.Config, error) {
+	if err := godotenv.Load(envFile); err != nil {
+		logger.
+			WithError(err).
+			Error("cannot load .env file:")
+	}
+
+	cfg, err := parse()
+	if err != nil {
+		logger.
+			WithError(err).
+			Error("cannot get config")
+		return nil, nil, nil, err
+	}
+
+	pgxConfig, err := pgxpool.ParseConfig(
+		fmt.Sprintf("postgres://%s:%s@%s:%s/%s",
+			cfg.Postgres.Username,
+			cfg.Postgres.Password,
+			cfg.Postgres.Host,
+			cfg.Postgres.Port,
+			cfg.Postgres.Database,
+		),
+	)
+	if err != nil {
+		logger.
+			WithError(err).
+			Error("cannot parse postgres config")
+		return nil, nil, nil, err
+	}
+	pgxConfig.ConnConfig.Tracer = &tracelog.TraceLog{
+		Logger:   pgxLogrus.NewLogger(logger.WithField("FROM", "[PGX-POOL]")),
+		LogLevel: tracelog.LogLevelDebug,
+	}
+
+	pool, err := pgxpool.NewWithConfig(context.Background(), pgxConfig)
+	if err != nil {
+		logger.
+			WithError(err).
+			Error("cannot create pgx pool")
+		return nil, nil, nil, err
+	}
+	postgresConfig := &postgres.Config{
+		Pool:   pool,
+		Logger: logger.WithField("FROM", "[POSTGRES]"),
+	}
+
+	kafkaConfig := &kafka.Config{
+		Brokers: strings.Split(cfg.Kafka.Brokers, ","),
+		Topics:  strings.Split(cfg.Kafka.Topics, ","),
+		GroupID: cfg.Kafka.GroupID,
+		Logger:  logger.WithField("FROM", "[KAFKA-CONSUMER]"),
+	}
+	redisConfig := &rds.Config{
+		Opt: &redis.Options{
+			Addr: fmt.Sprintf("%s:%s", cfg.Redis.Host, cfg.Redis.Port),
+			DB:   cfg.Redis.DB,
+		},
+		Key:    cfg.Redis.Key,
+		Logger: logger.WithField("FROM", "[REDIS]"),
+	}
+
+	return postgresConfig, kafkaConfig, redisConfig, nil
+}
+
+func parse() (*config, error) {
+	k, err := getKafkaConfig()
 	if err != nil {
 		return nil, err
 	}
-	postgres, err := getPostgresConfig()
+	p, err := getPostgresConfig()
 	if err != nil {
 		return nil, err
 	}
-	redis, err := getRedisConfig()
+	r, err := getRedisConfig()
 	if err != nil {
 		return nil, err
 	}
-	return &Config{
-		Kafka:    kafka,
-		Postgres: postgres,
-		Redis:    redis,
+	return &config{
+		Kafka:    k,
+		Postgres: p,
+		Redis:    r,
 	}, nil
 }
